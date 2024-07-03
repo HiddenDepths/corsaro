83094e474877b45589b3c5d03ba9ab3b363cc634
Leo Turnbull <me@leoturnbull.com>
2024-07-03 23:15:46 +0000

    first commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const nav = document.getElementById("nav");
+nav.addEventListener("click", function(evt) {
+    if (evt.target.tagName == "DIV" || evt.target.tagName == "A") {
+        nav.setAttribute("data-nav", (nav.getAttribute("data-nav") === "true") ? false : true)
+    }
+})
+
+
+window.addEventListener("scroll", function(evt) {
+    nav.setAttribute("data-bg-visible", (window.scrollY || window.pageYOffset) ? true : false)
+})
+
+document.querySelector("#menu-overlay > button").addEventListener("click", function() {
+    window.open("menu.jpg")
+})
